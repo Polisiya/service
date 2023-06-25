75a70b1288b1ee57b561eaf696823ea1cb968aa4
Polina <polisiya@list.ru>
2023-06-25 14:47:28 +0300

    add ignoreMain

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int pricePerMile = 20;
+        int bonusPerMile = 1;
+        int amountOfBonus = ticketPrice / pricePerMile * bonusPerMile;
+        System.out.println(amountOfBonus = ticketPrice / pricePerMile * bonusPerMile);
+    }
+}
